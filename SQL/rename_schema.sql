f90b655981b1a69c817b458963943e114b472aae
Chaves <Alison.Chaves@thomsonreuters.com>
2017-10-04 13:33:44 -0300

    adicionando select para retorno dos schemas que precisam ser alterados

--- SQL/rename_schema.sql
+++ SQL/rename_schema.sql
@@ -1,3 +1,12 @@
+SELECT 'ALTER SCHEMA dbo TRANSFER [' + SysSchemas.Name + '].[' + DbObjects.Name + '];'
+FROM sys.Objects DbObjects
+INNER JOIN sys.Schemas SysSchemas ON DbObjects.schema_id = SysSchemas.schema_id
+WHERE SysSchemas.Name = 'TED'
+AND (DbObjects.Type IN ('U', 'P', 'V'))
+
+
+-------
+
 
 DECLARE @SchemaAtual AS VARCHAR(50);
 DECLARE @SchemaNovo AS VARCHAR(50);
